ba2ba285352f55bb0c0873fb01dabbf2be4f365a
GoldenShoulder <yunj3153@gmail.com>
2022-10-12 18:12:51 +0900

    make list
    절대경로 list 만들었습니다.

--- labelcounter.py
+++ labelcounter.py
@@ -13,6 +13,22 @@ CLASSES = [
 # 3. make lists ()
 
 
+def make_xmlist(dir):
+        xmlist =[]
+        filenames = os.listdir(dir)
+        for filename in filenames:
+            name = os.path.join(dir,filename)
+            if os.path.isdir(name):
+                temp =print_file(name)
+                xmlist+=temp
+            else:
+                ext = os.path.splitext(name)[-1]
+                if ext == '.xml':
+                    print(name)
+                    xmlist.append(name)
+                    
+        return xmlist
+
 def label_counter(xml_dir):
     for f in os.listdir(xml_dir):
         if '.xml' in f:
