6aabefdd42f5171b83b80d501778eaca452d6778
hrkimhgu <92333658+hrkimhgu@users.noreply.github.com>
2022-10-19 12:04:19 +0900

    Update labelcounter.py

--- labelcounter.py
+++ labelcounter.py
@@ -8,65 +8,9 @@ import numpy as np
 import multiprocessing as mp
 import parmap
 
-
-def label_counter(xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6):
-    for f in xmlist:
-        convert_label(f, sh0, sh1, sh2, sh3, sh4, sh5, sh6)
-
-def convert_label(in_file_name, sh0, sh1, sh2, sh3, sh4, sh5, sh6):
-    in_file = open(in_file_name, 'r')
-    xml_dict = xmltodict.parse(in_file.read())
-    in_file.close()
-    size = xml_dict['annotation'].get('size', None)
-    objs = xml_dict['annotation'].get('object', None)
-
-    if not objs:
-        sh0.append(-1)
-        sh1.append(-1)
-        sh2.append(-1)
-        sh3.append(-1)
-        sh4.append(size['width'])
-        sh5.append(size['height'])
-        sh6.append(in_file_name)
-    elif type(objs) == list:
-        for obj in objs:
-            cl, bw, bh, bs = parse_obj(obj)
-            sh0.append(cl)
-            sh1.append(bw)
-            sh2.append(bh)
-            sh3.append(bs)
-            sh4.append(size['width'])
-            sh5.append(size['height'])
-            sh6.append(in_file_name)
-    else:
-        cl, bw, bh, bs = parse_obj(objs)
-        sh0.append(cl)
-        sh1.append(bw)
-        sh2.append(bh)
-        sh3.append(bs)
-        sh4.append(size['width'])
-        sh5.append(size['height'])
-        sh6.append(in_file_name)
-
-def make_xmlist(dir):
-    xmlist = []
-    for path, dir, files in os.walk(dir):
-        xmlist += glob.glob(os.path.join(path, '*.xml'))
-    return xmlist
-
-def parse_obj(obj):
-    cl = obj['name']
-    bw = int(obj['bndbox']['xmax']) - int(obj['bndbox']['xmin'])
-    bh = int(obj['bndbox']['ymax']) - int(obj['bndbox']['ymin'])
-    bs = bw * bh
-    return cl, bw, bh, bs
-
-if __name__ == '__main__':
-    ## input the target directory (ex. set_k_train)
-    start = time.time()
-
+def label_counter(dir_name):
     print("[INFO] Searching xml directories...\n")
-    xmlist = np.array(make_xmlist(sys.argv[1]))
+    xmlist = np.array(make_xmlist(dir_name))
     num_cores = mp.cpu_count()
     splited_xmlist = np.array_split(xmlist, num_cores)
     splited_xmlist = [x.tolist() for x in splited_xmlist]
@@ -76,7 +20,7 @@ if __name__ == '__main__':
         globals()['sh{}'.format(i)] = mp.Manager().list()
 
     print("[INFO] Use {} cores of CPU".format(num_cores))
-    result = parmap.map(label_counter, splited_xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6, pm_pbar=True, pm_processes=num_cores)
+    result = parmap.map(convert_label, splited_xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6, pm_pbar=True, pm_processes=num_cores)
 
     print("[INFO] Converting to DataFrame...\n")
     dsh = dict()
@@ -87,14 +31,74 @@ if __name__ == '__main__':
     xml_df = pd.DataFrame(dsh)
 
     ## save
-    file_name = sys.argv[1]
-    xml_df.to_csv('{}.csv'.format(file_name))
-    print("[INFO] Saved file: {}/{}.csv\n".format(os.getcwd(), file_name))
+    xml_df.to_csv('{}.csv'.format(dir_name))
+    print("[INFO] Saved file: {}/{}.csv\n".format(os.getcwd(), dir_name))
 
-    ## print output !
-    print("\n---------------------------------------\n")
-    print("Total bndbox : {}\n".format(xml_df['class'].count()))
-    print("Class count\n", xml_df['class'].value_counts())
-    print("\n", xml_df.head())
+def make_xmlist(dir):
+    xmlist = []
+    for path, dir, files in os.walk(dir):
+        xmlist += glob.glob(os.path.join(path, '*.xml'))
+    return xmlist
+
+def convert_label(xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6):
+    for in_file_name in xmlist:
+        in_file = open(in_file_name, 'r')
+        xml_dict = xmltodict.parse(in_file.read())
+        in_file.close()
+        size = xml_dict['annotation'].get('size', None)
+        objs = xml_dict['annotation'].get('object', None)
+
+        if not objs:
+            sh0.append(-1)
+            sh1.append(-1)
+            sh2.append(-1)
+            sh3.append(-1)
+            sh4.append(size['width'])
+            sh5.append(size['height'])
+            sh6.append(in_file_name)
+        elif type(objs) == list:
+            for obj in objs:
+                cl, bw, bh, bs = parse_obj(obj)
+                sh0.append(cl)
+                sh1.append(bw)
+                sh2.append(bh)
+                sh3.append(bs)
+                sh4.append(size['width'])
+                sh5.append(size['height'])
+                sh6.append(in_file_name)
+        else:
+            cl, bw, bh, bs = parse_obj(objs)
+            sh0.append(cl)
+            sh1.append(bw)
+            sh2.append(bh)
+            sh3.append(bs)
+            sh4.append(size['width'])
+            sh5.append(size['height'])
+            sh6.append(in_file_name)
+
+def parse_obj(obj):
+    cl = obj['name']
+    bw = int(obj['bndbox']['xmax']) - int(obj['bndbox']['xmin'])
+    bh = int(obj['bndbox']['ymax']) - int(obj['bndbox']['ymin'])
+    bs = bw * bh
+    return cl, bw, bh, bs
+
+def loader(file_name):
+    xml_df = pd.read_csv(file_name)
+    
+    print("**Data : {}\n".format(file_name))
+    print("**Total bndbox : {}\n".format(xml_df['class'].count()))
+    print("**Class count\n", xml_df['class'].value_counts())
     print("\n", xml_df.describe())
-    print("Overall time : {} s".format(round(time.time() - start, 3)))
+
+if __name__ == '__main__':
+    ## input the target directory (ex. set_k_train)
+    start = time.time()
+    if (sys.argv[1] == 'search') & (len(sys.argv) == 3):
+        label_counter(sys.argv[2])
+        print("[INFO] Overall time : {} s".format(round(time.time() - start, 3)))
+    elif (sys.argv[1] == 'load') & (len(sys.argv) == 3):
+        loader(sys.argv[2])
+        print("[INFO] Overall time : {} s".format(round(time.time() - start, 3)))
+    else:
+        print("Usage: labelcounter.py (search 'directory' / load 'file_name.csv')")
