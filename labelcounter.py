2473626891b1b8e039eb44f0e80436a264760888
hrkimhgu <92333658+hrkimhgu@users.noreply.github.com>
2022-10-20 02:22:04 +0900

    Update labelcounter.py

--- labelcounter.py
+++ labelcounter.py
@@ -35,20 +35,14 @@ def label_counter(dir_name):
     splited_xmlist = [x.tolist() for x in splited_xmlist]
 
     print("[INFO] Preparing to collect data...\n")
-    for i in range(7):
-        globals()['sh{}'.format(i)] = mp.Manager().list()
+    sh = mp.Manager().list()
 
     print("[INFO] Use {} cores of CPU".format(num_cores))
-    result = parmap.map(convert_label, splited_xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6, pm_pbar=True, pm_processes=num_cores)
-
-    print("[INFO] Converting to DataFrame...\n")
-    dsh = dict()
-    keys = ['class', 'box_w', 'box_h', 'box_s', 'img_w', 'img_h', 'dir']
-    for i, l in enumerate([sh0, sh1, sh2, sh3, sh4, sh5, sh6]):
-        dsh[keys[i]] = l.__deepcopy__({})
+    result = parmap.map(convert_label, splited_xmlist, sh, pm_pbar=True, pm_processes=num_cores)
     
-    xml_df = pd.DataFrame(dsh)
-
+    print("[INFO] Converting to DataFrame...\n")
+    xml_df = pd.DataFrame(list(sh), columns=['class', 'box_w', 'box_h', 'box_s', 'img_w', 'img_h', 'dir'])
+    
     xml_df.to_csv('{}.csv'.format(dir_name))
     print("[INFO] Saved file: {}/{}.csv\n".format(os.getcwd(), dir_name))
 
@@ -58,7 +52,7 @@ def make_xmlist(dir):
         xmlist += glob.glob(os.path.join(path, '*.xml'))
     return xmlist
 
-def convert_label(xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6):
+def convert_label(xmlist, sh):
     for in_file_name in tqdm(xmlist):
         in_file = open(in_file_name, 'r')
         xml_dict = xmltodict.parse(in_file.read())
@@ -67,32 +61,14 @@ def convert_label(xmlist, sh0, sh1, sh2, sh3, sh4, sh5, sh6):
         objs = xml_dict['annotation'].get('object', None)
 
         if not objs:
-            sh0.append(-1)
-            sh1.append(-1)
-            sh2.append(-1)
-            sh3.append(-1)
-            sh4.append(size['width'])
-            sh5.append(size['height'])
-            sh6.append(in_file_name)
+            sh.append([-1, -1, -1, -1, size['width'], size['height'], in_file_name])
         elif type(objs) == list:
             for obj in objs:
                 cl, bw, bh, bs = parse_obj(obj)
-                sh0.append(cl)
-                sh1.append(bw)
-                sh2.append(bh)
-                sh3.append(bs)
-                sh4.append(size['width'])
-                sh5.append(size['height'])
-                sh6.append(in_file_name)
+                sh.append([cl, bw, bh, bs, size['width'], size['height'], in_file_name])
         else:
             cl, bw, bh, bs = parse_obj(objs)
-            sh0.append(cl)
-            sh1.append(bw)
-            sh2.append(bh)
-            sh3.append(bs)
-            sh4.append(size['width'])
-            sh5.append(size['height'])
-            sh6.append(in_file_name)
+            sh.append([cl, bw, bh, bs, size['width'], size['height'], in_file_name])
 
 def parse_obj(obj):
     cl = obj['name']
